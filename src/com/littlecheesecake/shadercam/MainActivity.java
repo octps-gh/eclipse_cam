617dbcea4832c7856a99d64a995521bbf77c006a
mukr.cheung <mukr.zhang@spreadtrum.com>
2016-05-27 19:10:14 +0800

    update resource

--- src/com/littlecheesecake/shadercam/MainActivity.java
+++ src/com/littlecheesecake/shadercam/MainActivity.java
@@ -2,15 +2,23 @@ package com.littlecheesecake.shadercam;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.Window;
 import android.view.WindowManager;
 
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.littlecheesecake.shadercam.gl.CameraRenderer;
 import com.littlecheesecake.shadercameraexample.R;
 
+
+
 public class MainActivity extends Activity {
 	private CameraRenderer mRenderer;
+	ImageButton imgBtnVert;
+	ImageButton imgBtnHori;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +30,31 @@ public class MainActivity extends Activity {
 
 		setContentView(R.layout.activity_main);
 		mRenderer = (CameraRenderer)findViewById(R.id.renderer_view);
+		
+		addListenerOnButton();
 	}
 
+	public void addListenerOnButton() {
+
+		imgBtnVert = (ImageButton) findViewById(R.id.ibtn_vert);
+		imgBtnHori = (ImageButton) findViewById(R.id.ibtn_hori);
+		imgBtnVert.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View arg0) {
+			   Toast.makeText(MainActivity.this,"Vertical ImgButton is clicked!", Toast.LENGTH_SHORT).show();
+			}
+		});
+		
+		imgBtnHori.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View arg0) {
+			   Toast.makeText(MainActivity.this,"Horizontal ImgButton is clicked!", Toast.LENGTH_SHORT).show();
+			}
+		});
+		
+
+	}
+	
 	@Override
 	public void onStart(){
 		super.onStart();
